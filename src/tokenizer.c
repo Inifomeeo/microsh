0536170b46b446d63ec6e30d76dc72cce1cedaec
78764279+Inifomeeo@users.noreply.github.com <78764279+Inifomeeo@users.noreply.github.com>
2025-03-20 22:16:34 +0000

    refactor: ensure tokens has enough space for each token

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -5,20 +5,31 @@
 
 #include "tokenizer.h"
 
+// Split input into tokens separated by a space
 char **tokenize_input(char *input)
 {
-    char **tokens = malloc(10 * sizeof(char*));
+    int buf_size = 10;
+    int index = 0;
+    char **tokens = malloc(buf_size * sizeof(char *));
     if (tokens == NULL) {
         perror("malloc");
         exit(1);
     }
     char *token;
-    int index = 0;
 
     token = strtok(input, " ");
     while (token != NULL) {
         tokens[index] = token;
         index++;
+
+        if (index >= buf_size) {
+            buf_size += buf_size;
+            tokens = realloc(tokens, buf_size * sizeof(char *));
+            if (tokens == NULL) {
+                perror("realloc");
+                exit(1);
+            }
+        }
 
         token = strtok(NULL, " ");
     }
