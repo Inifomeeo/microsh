205b3fc437469e6cd5c9c5db841584a8d748a404
78764279+Inifomeeo@users.noreply.github.com <78764279+Inifomeeo@users.noreply.github.com>
2025-03-20 22:20:14 +0000

    feat: allow execution of commands

--- src/main.c
+++ src/main.c
@@ -2,12 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "tokenizer.h"
 
-int main() {
+int main()
+{
     char *input;
     char **command;
+    pid_t pid;
+    int buf_size = 128;
+    int stat_loc;
     
     while (1)
     {
@@ -15,14 +22,14 @@ int main() {
         printf("$ ");
         fflush(stdout);
 
-        input = malloc(128 * sizeof(char));
+        input = malloc(buf_size * sizeof(char));
         if (input == NULL) {
             perror("malloc");
             exit(1);
         }
 
         // Wait for user input
-        fgets(input, 128, stdin);
+        fgets(input, buf_size, stdin);
         input[strcspn(input, "\r\n")] = 0;
 
         command = tokenize_input(input);
@@ -30,6 +37,19 @@ int main() {
             free(input);
             free(command);
             continue;
+        }
+
+        pid = fork();
+        if (pid == 0) {
+            if (execvp(command[0], command) < 0) {
+                perror("execvp");
+                exit(1);
+            }
+        } else if (pid < 0) {
+            perror("fork");
+            exit(1);
+        } else {
+            waitpid(pid, &stat_loc, WUNTRACED);
         }
 
         free(input);
